ebccd9191e1780fc3f00fe1b65ac7164edca9c1b
Titou <thibault.dulon@gmail.com>
2012-12-10 22:39:24 +0100

    meta og

--- portail/frontend/themes/default/partials/meta.php
+++ portail/frontend/themes/default/partials/meta.php
@@ -1,20 +1,26 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" >
-<meta name="Description" content="" >
-<meta name="Author" content="Webtuts, fitz_lucassen">
+<meta name="Description" content="Webtuts, un blog de tutoriels visant à enseigner la réalisation de son propre site web grâces à diverses astuces et techniques expliquées de façon détaillés. Le développement de ce même blog sera, le plus souvent possible, prit comme exemple à ces tutoriaux." >
+<meta name="Author" content="Webtuts, fitz_lucassen, Fozeek, Musha, Deak, Thibault, Quentin, Jonathan, Richard">
 <meta name="Identifier-URL" content="http://webtuts.fr">
 <meta name="Date-Creation-yyyymmdd" content="20130101">
 <meta name="Date-Revision-yyyymmdd" content="20130101">
 <meta name="Reply-to" content="contact@webtuts.fr">
 <meta name="revisit-after" content="15 days">
 <meta name="Category" content="Internet">
-<meta name="Publisher" content="Thibault DULON">
-<meta name="Copyright" content="Thibault DULON">
+<meta name="Publisher" content="Webtuts, Thibault DULON, Quentin DENEUVE, Richard ETTOU, Jonathan BICHEUX">
+<meta name="Copyright" content="Webtuts">
 <meta name="Generator" content="PHP Engineer, HTML5/CSS3 Integrator">
 <meta name="robots" content="index, follow, all">
 <meta name="Rating" content="general">
 <meta name="Distribution" content="global">
 <meta name="Geography" content="Paris, France, 75019">
+
+<meta property="og:title" content="Webtuts" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://webtuts.fr" />
+<meta property="og:image" content="<?php echo _host_absolute_ . _theme_path_ ?>images/favicon.gif" />
+<meta property="og:description" content="Webtuts, un blog de tutoriels visant à enseigner la réalisation de son propre site web grâces à diverses astuces et techniques expliquées de façon détaillés. Le développement de ce même blog sera, le plus souvent possible, prit comme exemple à ces tutoriaux." />
 
 <link rel="icon" type="images/png" href="<?php echo _host_absolute_ . _theme_path_ ?>images/favicon.ico" />
 <link type="text/css" rel="stylesheet" href="<?php echo _host_absolute_ . _theme_path_ ?>css/style.css" />
