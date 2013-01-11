0353aa4eb76deb8096f679cac611bb36511a769d
Quentin <dark.quent@free.fr>
2013-01-11 15:41:00 +0100

    charset utf8 db

--- site/kernel/db_co.php
+++ site/kernel/db_co.php
@@ -8,6 +8,7 @@
 
 	mysql_connect($_CONFIG_sql_hostname, $_CONFIG_sql_user,$_CONFIG_sql_password);
 	mysql_select_db(__SQL_db__);
+	mysql_set_charset("utf8");
 
 	$_CONFIG_sql_hostname = "";
 	$_CONFIG_sql_user = "";
