b2d926df93a7d2c8b26c231e3582b2bf3446af7c
Muusha <bicheuxj@gmail.com>
2013-01-11 15:23:15 +0100

    Update Backend

--- site/backend/themes/default/pages/node/show.php
+++ site/backend/themes/default/pages/node/show.php
@@ -1,1 +1,33 @@
-<?php partial("pageHeader"); ?>
+<div style="padding :20px;">
+	<div style="padding-bottom: 10px;border-bottom: 1px solid #E5E5E5;margin-bottom: 20px;">
+		<div style="font-size: 1.6em;float: left;">
+			<span style="color: grey;"><?php echo ucfirst(text("node")); ?> :</span> 
+			<?php echo minifyTitle(lang($node->get("name"))); ?>
+		</div>
+		<div style="overflow: hidden;padding-top:10px;padding-left: 20px;">
+			<a href="<?php echo createLink("/node/show/".$node->get("id")); ?>" style="display: inline-block;padding-right: 5px;padding-left: 5px;"><?php echo ucfirst(text("description")); ?></a>
+			<a href="<?php echo createLink("/node/update/".$node->get("id")); ?>" style="display: inline-block;padding-right: 5px;padding-left: 5px;"><?php echo ucfirst(text("update")); ?></a>
+			<a href="<?php echo createLink("/node/delete/".$node->get("id")); ?>" style="display: inline-block;padding-right: 5px;padding-left: 5px;"><?php echo ucfirst(text("delete")); ?></a>
+		</div>
+		<div style="clear: both;">
+		</div>
+	</div>
+
+	<div style="float: left;width: 200px;padding: 15px;font-weight: bold;">
+		<?php echo ucfirst(text("name")); ?>
+	</div>
+	<div style="overflow: hidden;padding: 15px;">
+		<?php echo lang($node->get("name")); ?>
+	</div>
+	<div style="clear: left;">
+	</div>
+
+	<div style="float: left;width: 200px;padding: 15px;font-weight: bold;">
+		<?php echo ucfirst(text("description")); ?>
+	</div>
+	<div style="overflow: hidden;padding: 15px;">
+		<?php echo lang($node->get("description")); ?>
+	</div>
+	<div style="clear: left;">
+	</div>
+</div>
