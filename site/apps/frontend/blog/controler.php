d5f0d089053b3c1de158060340884c56129502da
Titou <thibault.dulon@gmail.com>
2013-01-21 12:00:05 +0100

    maj url rewwrite + cotroler

--- site/apps/frontend/blog/controler.php
+++ site/apps/frontend/blog/controler.php
@@ -14,15 +14,15 @@ class BlogControler extends Controler {
 		return $this->render(array('cat' => $params[3]));
 	}
 	public function ArticlesAction($params) {
-		$news = App::getClassArray("article", array("where" => "node = 1"));
-		$articles = App::getClassArray("article", array("where" => array("have" => "category")));
+		$news = App::getClassArray("article", array("where" => "node = 2"));
+		$articles = App::getClassArray("article", array("where" => "node = 1"));
 		
 		return $this->render(array('articles' => $articles, "news" => $news));
 	}
 
 	public function CategoriesAction($params) {
 		$cats = App::getClassArray("category");
-		$news = App::getClassArray("article", array("where" => "node = 1"));
+		$news = App::getClassArray("article", array("where" => "node = 2"));
 		
 		return $this->render(array('cats' => $cats, "news" => $news));
 	}
--- site/apps/frontend/home/controler.php
+++ site/apps/frontend/home/controler.php
@@ -2,10 +2,10 @@
 
 class HomeControler extends Controler {
 	public function IndexAction($params) {
-	    $cats = App::getClassArray("category");
+	    $cats = App::getClassArray("category", array("limit" => 6));
 	    
-	    $news = App::getClassArray("article", array("where" => "node = 1", "limit" => 5));
-	    $articles = App::getClassArray("article", array("where" => array("have" => "category"), "limit" => 5));
+	    $news = App::getClassArray("article", array("where" => "node = 2", "limit" => 5));
+	    $articles = App::getClassArray("article", array("where" => "node = 1", "limit" => 5));
 	    
 	    return $this->render(array('cats' => $cats, 'articles' => $articles, 'news' => $news));
 	}
