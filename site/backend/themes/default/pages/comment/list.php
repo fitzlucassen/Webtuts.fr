a4934305d458d11373cee57bf64e7e78c7a1917b
Muusha <bicheuxj@gmail.com>
2013-01-22 11:10:24 +0100

    Maj backend

--- site/backend/themes/default/pages/comment/list.php
+++ site/backend/themes/default/pages/comment/list.php
@@ -8,18 +8,22 @@
 	</div>
 
 	<?php foreach ($comments as $comment) : ?>
-	<a href="<?php echo createLink("/comment/show/".$comment->get("id")); ?>">
-	<div style="border-bottom: 1px solid #E5E5E5;padding: 15px;">	
-		<div style="float: left;width: 200px;">
-			<?php echo $comment->get("author")->get("pseudo"); ?>
+	<div class="comment" style="border-bottom: 1px solid #E5E5E5;padding: 15px;cursor: pointer;">	
+		<div class="titleitem" style="float: left;width: 200px;">
+			<a href="<?php echo createLink("/user/show/".$comment->get("author")->get("id")); ?>"><?php echo $comment->get("author")->get("pseudo"); ?></a>
+			<span style="display: none;font-size: 0.8em;"><?php echo "Le ".printdate($comment->get("date")); ?></span>
 		</div>
-		<div style="overflow: hidden;padding-top: 1px;padding-left: 10px;">
-			<span style="font-size: 0.8em;color: grey;"><?php echo $comment->get("text"); ?></span>
+		<div class="descriptionitem" style="overflow: hidden;padding-top: 1px;padding-left: 10px;position: relative;">
+			<span><?php echo lang($comment->get("article")->get("title")); ?></span>
+			<span class="textcomment" style="margin-top: 10px;font-size: 0.8em;color: grey;display :none;"><?php echo $comment->get("text"); ?></span>		
+			<form method="post" action=<?php echo createLink("/comment/delete"); ?>>
+				<input type="hidden" name="id" value="<?php echo $comment->get("id"); ?>"/>
+				<input type="submit" value="Supprimer" style="display: none; margin-top: 10px;" />
+			</form>
 		</div>
 		<div style="clear: both;">
 		</div>
 	</div>
-	</a>
 	<?php endforeach; ?>
 
 </div>
