2627822b4bdfb0daee723cb01994f195f70e9dd6
Quentin <dark.quent@free.fr>
2013-01-23 17:01:41 +0100

    MaJ

--- site/library/resources/ormstdtableabstract.class.php
+++ site/library/resources/ormstdtableabstract.class.php
@@ -6,6 +6,7 @@ abstract class OrmStdTableAbstract {
 	public $_name;
 	public $_collection;
 	public $_types;
+	public static $CACHE;
 
 	public function __construct($name = null) {
 		if(empty($name)) {
@@ -16,27 +17,50 @@ abstract class OrmStdTableAbstract {
 		$this->_name = $name;
 	}
 
+	public function getCache() {
+		if(OrmStdTableAbstract::$CACHE==null)
+			OrmStdTableAbstract::$CACHE = new Cache(Cache::getDir()."orm", 60);
+		return OrmStdTableAbstract::$CACHE;
+	}
+
 	public function count() {
 		return $this->_collection->count();
 	}
 
-	public function getCollection() {
+	public function get() {
 		if(empty($this->_collection))
 			$this->_collection = Sql2::create()->from($this->_name)->fetchClassArray();
 		return $this->_collection;
 	}
 
 	public function getTypes() {
-		return null;
+		if(empty($this->_types)) {
+			$Cache = $this->getCache();
+			if(!$types = $Cache->read("ORM_table_".$this->_name)) {
+				$types = Sql2::create()->from("ORM_columns_types")->where("name_table", Sql2::$OPE_EQUAL, mb_strtolower($this->_name))->fetchArray();
+				$Cache->write("ORM_table_".$this->_name, serialize($types));
+			} 
+			else
+				$types = unserialize($types);
+			foreach ($types as $value) {
+				$this->_types[$value["name_column"]] = $value["type"];
+			}
+		}
+		return $this->_types;
 	}
 
 	public function __call($name, $params) {
 		if(strstr($name, "getBy")) {
 			$return = new Collection();
 			$attribut = strtolower(substr($name, 5));
-			foreach ($this->getCollection() as $object) {
-				if($object->get($attribut)==$params[0])
-					$return->hydrate($object);
+			if(!strstr($name, "Sanitize")) {
+				foreach ($this->getCollection() as $object) {
+					if($object->get($attribut)==$params[0])
+						$return->hydrate($object);
+				}
+			}
+			else {
+				
 			}
 			return $return;
 		}
